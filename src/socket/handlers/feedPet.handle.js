7608b939b196361f13d86a3c61bfc870fdd7979f
bui huy hoang <buihoang34252@gmail.com>
2025-07-16 17:11:03 +0700

    fix(pet):feedPet

--- src/socket/handlers/feedPet.handle.js
+++ src/socket/handlers/feedPet.handle.js
@@ -4,3 +4,7 @@ const feedPet = (io, receiverId, data) => {
   const socketReceiverId = usersOnline.getSocketId(receiverId)
   io.to(socketReceiverId).emit('SERVER_FEED_PET_SUCCESS', data)
 }
+
+module.exports = {
+  feedPet
+}
