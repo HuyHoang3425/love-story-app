55334f4f43f91aeb5496cfe34e7ecdfddff7c154
bui huy hoang <buihoang34252@gmail.com>
2025-07-26 01:10:45 +0700

    fix(pet): socket feedpet

--- src/socket/handlers/feedPet.handle.js
+++ src/socket/handlers/feedPet.handle.js
@@ -1,8 +1,8 @@
 const { usersOnline, catchAsync } = require('../../utils')
 
 const feedPet = (io, receiverId, userId, data) => {
-  const socketReceiverId = usersOnline.getSocketId(receiverId)
-  const socketUserId = usersOnline.getSocketId(userId)
+  const socketReceiverId = usersOnline.getSocketId(receiverId.toString())
+  const socketUserId = usersOnline.getSocketId(userId.toString())
   io.to(socketReceiverId).emit('SERVER_FEED_PET_SUCCESS', data)
   io.to(socketUserId).emit('SERVER_FEED_PET_SUCCESS', data)
 }
