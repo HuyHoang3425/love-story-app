517140300dee331c81d028bdec8e6cf94a08720b
HuyHoang3425 <buihoang3425@gmail.com>
2025-08-05 00:54:06 +0700

    config firebase

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin')
-const serviceAccount = require('../json/love-story-app-4c8d7-firebase-adminsdk-fbsvc-2edad95461.json')
+const serviceAccount = require('../json/love-story-app-4c8d7-firebase-adminsdk-fbsvc-085a74a3e4.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
