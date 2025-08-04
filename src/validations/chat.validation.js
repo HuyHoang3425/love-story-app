fe47622fa98d58a9f3166381fbea0e048855a803
HuyHoang3425 <buihoang3425@gmail.com>
2025-08-04 14:47:27 +0700

    validate coupleId

--- src/validations/chat.validation.js
+++ src/validations/chat.validation.js
@@ -2,7 +2,8 @@ const joi = require('joi')
 
 const message = joi.object({
   senderId: joi.string().required(),
-  toUserId: joi.string().required(), 
+  toUserId: joi.string().required(),
+  coupleId: joi.string().required(),
   content: joi.string().max(2000).allow('', null),
   images: joi.array().items(joi.string()).default([])
 })
