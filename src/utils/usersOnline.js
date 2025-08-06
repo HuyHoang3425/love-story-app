aa785ba1de4c922eea7fc58f4dc2adbef1d50923
HuyHoang3425 <buihoang3425@gmail.com>
2025-08-06 23:24:40 +0700

    update Map socket

--- src/utils/usersOnline.js
+++ src/utils/usersOnline.js
@@ -1,20 +1,33 @@
-const onlineUsers = new Map()
+const onlineUsers = new Map() // Map<userId, Set<socketId>>
 
 function addUser(userId, socketId) {
-  onlineUsers.set(userId, socketId)
+  if (!onlineUsers.has(userId)) {
+    onlineUsers.set(userId, new Set())
+  }
+  onlineUsers.get(userId).add(socketId)
 }
 
 function removeUser(socketId) {
-  for (const [userId, sid] of onlineUsers) {
-    if (sid === socketId) {
-      onlineUsers.delete(userId)
+  for (const [userId, socketSet] of onlineUsers) {
+    if (socketSet.has(socketId)) {
+      socketSet.delete(socketId)
+      if (socketSet.size === 0) {
+        onlineUsers.delete(userId)
+      }
       break
     }
   }
 }
 
 function getSocketId(userId) {
-  return onlineUsers.get(userId)
+  const socketSet = onlineUsers.get(userId)
+  if (!socketSet || socketSet.size === 0) return null
+  return [...socketSet][0]
 }
 
-module.exports = { addUser, removeUser, getSocketId }
+module.exports = {
+  addUser,
+  removeUser,
+  getSocketId,
+  onlineUsers 
+}
